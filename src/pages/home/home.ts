b30e3303480afe216bce23775663b6832347482c
joshgressman <joshgressman@gmail.com>
2017-11-23 09:31:49 -0600

    added side bar with items

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,21 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { TabsPage } from '../tabs/tabs';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  @Input('logged') loggedIn: boolean = false;
+
+
   constructor(public navCtrl: NavController) {
 
   }
 
-  //How do you hide show the tabs on the tabs screen if the signin page has the sign in data
-  //
-  signIn(){
-    this.loggedIn = true
-  
-  }
 
 }
--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.html',
 })
 export class MenuPage {
+  homePage = HomePage;
+
+ @ViewChild('content') childNavCtrl: NavController;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
   }
 
   ionViewDidLoad() {
