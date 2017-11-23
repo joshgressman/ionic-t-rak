3f7a02c63ab9af9004336d57d711645597a1655c
joshgressman <joshgressman@gmail.com>
2017-11-23 08:46:14 -0600

    added menu page removed tabs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { TabsPage } from '../tabs/tabs';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @Input('logged') loggedIn: boolean = false;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  //How do you hide show the tabs on the tabs screen if the signin page has the sign in data
+  //
+  signIn(){
+    this.loggedIn = true
+  
   }
 
 }
