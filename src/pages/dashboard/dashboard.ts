2ea621147199b1f94296be7f4ed7dc5a25f3ee39
joshgressman <joshgressman@gmail.com>
2017-12-02 08:38:44 -0600

    storage display total

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -8,12 +8,20 @@ import { TransactionsService } from '../../services/transactions';
 })
 export class DashboardPage {
   transactions: any = []
+  total: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private transactionsService: TransactionsService) {
+   this.total = 0;
   }
 
   ionViewDidLoad() {
    this.transactions = this.transactionsService.loadTransactions();
+   console.log("trans", this.transactions[0].cost);
+
+   for(let i = 0; i < this.transactions.length; i++){
+     this.total += this.transactions[i].cost;
+     console.log("hi", this.total);
+   }
   }
 
 }
